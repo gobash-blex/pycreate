93d8e649d6dc498452686981d03d4d86b158bcba
gobash-blex <brian_ramos_14@yahoo.com>
2024-03-27 20:15:00 -0700

    Create the content generator file as well as content folder with mock markdown and an html template

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
-from textnode import TextNode
+import os
+import shutil
+
+from copy_content import copy_files_recursive
+from generate_content import generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 
 main()
