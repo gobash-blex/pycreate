49f8dc15daaa020a8176815ca391d2631a9c8603
gobash-blex <brian_ramos_14@yahoo.com>
2024-03-27 20:29:25 -0700

    Add functionality to generate multiple static pages from multiple markdown files

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from copy_content import copy_files_recursive
-from generate_content import generate_page
+from generate_content import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -18,11 +18,6 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
-
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 main()
